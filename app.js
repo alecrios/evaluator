88102e63d7cf265410772c7477df9c92609321e7
Alec Rios <8664387+alecrios@users.noreply.github.com>
2018-08-11 12:42:43 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow, globalShortcut} = require('electron');
+const url = require('url');
+const path = require('path');
+
+let win = null;
+
+const createWindow = () => {
+	win = new BrowserWindow({show: false});
+
+	win.once('ready-to-show', () => {win.show()})
+
+	win.loadFile('index.html');
+
+	win.on('closed', () => {win = null});
+};
+
+app.on('ready', () => {
+	createWindow();
+
+	globalShortcut.register('CommandOrControl+X', () => {
+		win.focus();
+	});
+});
+
+app.on('will-quit', () => {
+	globalShortcut.unregisterAll();
+});
