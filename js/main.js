dff24e440817a2d9062527f7a7857f559e5143ec
Alec Rios <8664387+alecrios@users.noreply.github.com>
2018-08-11 23:53:46 -0500

    added keyboard behavior

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const {clipboard} = require('electron');
+
 class Calculator {
 	constructor() {
 		this._symbols = {};
@@ -145,29 +147,92 @@ class Row {
 		this.output.rows = '1';
 		this.output.classList.add('textarea', 'output');
 		this.output.setAttribute('readonly', '');
+		this.output.tabIndex = -1;
 		this.calculation.appendChild(this.output);
 
-		this.input.focus();
-
-		this.input.addEventListener('input', this.handleInput.bind(this));
-
-		this.output.addEventListener('focus', () => {
-			this.output.select();
-		});
+		this.addEventListeners();
 
 		this.calculator = new Calculator();
-	}
 
-	handleInput() {
-		this.updateHeight(this.input);
-		this.updateHeight(this.output);
-
-		this.evaluate();
+		this.input.focus();
 	}
 
 	updateHeight(textarea) {
 		textarea.style.height = '0px';
 		textarea.style.height = textarea.scrollHeight + 'px';
+	}
+
+	addEventListeners() {
+		this.input.addEventListener('input', () => {
+			this.updateHeight(this.input);
+			this.updateHeight(this.output);
+			this.evaluate();
+		});
+
+		this.input.addEventListener('keydown', (event) => {
+			switch (event.key) {
+				case 'Enter':
+					if (event.shiftKey) return;
+					event.preventDefault();
+					if (!this.output.value) return;
+					this.output.focus();
+					break;
+				case 'Tab':
+					if (event.shiftKey) return;
+					this.goToNextRow(event);
+					break;
+			}
+		});
+
+		this.output.addEventListener('keydown', (event) => {
+			switch (event.key) {
+				case 'Enter':
+					event.preventDefault();
+					clipboard.writeText(this.output.value);
+					break;
+				case 'Tab':
+					if (event.shiftKey) return;
+					this.goToNextRow(event);
+					break;
+			}
+		});
+
+		this.input.addEventListener('focus', () => {
+			this.updateActiveStatus();
+		});
+
+		this.input.addEventListener('blur', () => {
+			this.updateActiveStatus();
+		});
+
+		this.output.addEventListener('focus', () => {
+			this.updateActiveStatus();
+			this.output.select();
+		});
+
+		this.output.addEventListener('blur', () => {
+			this.updateActiveStatus();
+		});
+	}
+
+	updateActiveStatus() {
+		if (document.activeElement === this.input || document.activeElement === this.output) {
+			this.calculation.classList.add('active');
+		} else {
+			this.calculation.classList.remove('active');
+		}
+	}
+
+	isLastRow() {
+		return this.calculations.lastChild === this.calculation;
+	}
+
+	goToNextRow() {
+		if (!this.isLastRow()) return;
+		event.preventDefault();
+		if (!this.input.value) return;
+		new Row();
+		return;
 	}
 
 	evaluate() {
