4a7298cca2233e581363c31769d7c8fc9fbdbe62
Alec Rios <8664387+alecrios@users.noreply.github.com>
2018-10-10 02:30:53 -0500

    Changed phrasing of update button, added class

--- js/update.js
+++ js/update.js
@@ -2,7 +2,8 @@ const {ipcRenderer} = require('electron');
 
 ipcRenderer.on('updateReady', function() {
 	const button = document.createElement('button');
-	button.innerHTML = 'Update available';
+	button.innerHTML = 'Update ready';
+	button.classList.add('update-ready');
 	button.addEventListener('click', () => ipcRenderer.send('quitAndInstall'));
 	document.body.appendChild(button);
 });
