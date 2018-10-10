4b81935d1e45ecb1365a957d16fa266b9675eaa4
Alec Rios <8664387+alecrios@users.noreply.github.com>
2018-10-10 03:06:02 -0500

    Added styling for the update button

--- js/update.js
+++ js/update.js
@@ -2,8 +2,9 @@ const {ipcRenderer} = require('electron');
 
 ipcRenderer.on('updateReady', function() {
 	const button = document.createElement('button');
+	const footer = document.querySelector('.footer');
 	button.innerHTML = 'Update ready';
 	button.classList.add('update-ready');
 	button.addEventListener('click', () => ipcRenderer.send('quitAndInstall'));
-	document.body.appendChild(button);
+	footer.appendChild(button);
 });
