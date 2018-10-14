9ca3e864006e884eab8e03f56191a2c67e1c4d6d
Alec Rios <8664387+alecrios@users.noreply.github.com>
2018-10-13 23:40:31 -0500

    Moved update script into main.js

--- app/js/main.js
+++ app/js/main.js
@@ -2,3 +2,13 @@ const CommandBus = require('../lib/CommandBus');
 const Calculator = require('../lib/calculator/Calculator');
 const Row = require('./js/Row');
 const Workspace = require('./js/Workspace');
+const {ipcRenderer} = require('electron');
+
+ipcRenderer.on('updateReady', function() {
+	const button = document.createElement('button');
+	const footer = document.querySelector('.footer');
+	button.innerHTML = 'Update ready';
+	button.classList.add('update-ready');
+	button.addEventListener('click', () => ipcRenderer.send('quitAndInstall'));
+	footer.appendChild(button);
+});
