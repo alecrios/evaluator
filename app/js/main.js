cae2da620792bd2c8bb7fbeac4f7e57fe9de4728
Alec Rios <8664387+alecrios@users.noreply.github.com>
2018-10-13 23:35:25 -0500

    Moved CommandBus to lib

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-const CommandBus = require('./js/CommandBus');
+const CommandBus = require('../lib/CommandBus');
 const Calculator = require('../lib/calculator/Calculator');
 const Row = require('./js/Row');
 const Workspace = require('./js/Workspace');
