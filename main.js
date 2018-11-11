1630449fd31a3920f9891cf0e809f18e0dbee946
Alec Rios <8664387+alecrios@users.noreply.github.com>
2018-11-11 02:40:17 -0600

    Removed the window

--- main.js
+++ main.js
@@ -89,58 +89,6 @@ const createModal = () => {
 	if (isDev) modal.toggleDevTools();
 };
 
-let win = null;
-
-const windowSettings = new Store({
-	fileName: 'windowSettings',
-	defaults: {
-		width: 320,
-		height: 640,
-		x: null,
-		y: null,
-	},
-});
-
-const createWindow = () => {
-	const {width, height, x, y} = windowSettings.get();
-
-	win = new BrowserWindow({
-		show: false,
-		title: 'Evaluator',
-		titleBarStyle: 'hiddenInset',
-		minWidth: 218,
-		minHeight: 109,
-		width,
-		height,
-		x,
-		y,
-	});
-
-	win.setMenu(null);
-
-	win.once('ready-to-show', () => win.show());
-
-	win.on('resize', () => {
-		const bounds = win.getBounds();
-		windowSettings.set('width', bounds.width);
-		windowSettings.set('height', bounds.height);
-	});
-
-	win.on('move', () => {
-		const position = win.getPosition();
-		windowSettings.set('x', position[0]);
-		windowSettings.set('y', position[1]);
-	});
-
-	win.on('closed', () => {
-		win = null;
-	});
-
-	win.loadFile('app/index.html');
-
-	if (isDev) win.toggleDevTools();
-};
-
 app.on('ready', () => {
 	createModal();
 
@@ -150,7 +98,7 @@ app.on('ready', () => {
 });
 
 autoUpdater.on('update-downloaded', () => {
-	win.webContents.send('updateReady');
+	modal.webContents.send('updateReady');
 });
 
 ipcMain.on('quitAndInstall', () => {
