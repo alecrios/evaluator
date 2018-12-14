53fcad6a004335f646fc15180c8db900be2c78ce
Alec Rios <8664387+alecrios@users.noreply.github.com>
2018-12-14 05:10:27 -0600

    Added tray

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const {app, BrowserWindow, ipcMain, globalShortcut} = require('electron');
+const {app, BrowserWindow, ipcMain, globalShortcut, Menu, Tray} = require('electron');
 const {autoUpdater} = require('electron-updater');
+const path = require('path');
 const isDev = require('electron-is-dev');
 
 let modal = null;
 let modalStatus = 'hidden';
+
+let tray = null;
 
 function createModal() {
 	const size = {width: 384, height: 128};
@@ -14,6 +17,7 @@ function createModal() {
 		width: size.width,
 		height: size.height,
 		center: true,
+		skipTaskbar: true,
 		alwaysOnTop: true,
 		fullscreenable: false,
 		maximizable: false,
@@ -73,7 +77,7 @@ function createModal() {
 
 	modal.on('blur', initiateHideModal);
 
-	globalShortcut.register('CommandOrControl+Space', toggleModal);
+	globalShortcut.register('CmdOrCtrl+Space', toggleModal);
 
 	app.on('activate', () => {
 		initiateShowModal();
@@ -88,6 +92,29 @@ function createModal() {
 	modal.loadFile('app/modal.html');
 
 	if (!isDev) autoUpdater.checkForUpdates();
+
+	if (app.dock) app.dock.hide();
+
+	tray = new Tray(path.join(app.getAppPath(), 'build', 'icon.png'));
+
+	tray.setContextMenu(Menu.buildFromTemplate([
+		{
+			label: 'Show Evaluator',
+			accelerator: 'CmdOrCtrl+Space',
+			click: () => {
+				initiateShowModal();
+			},
+		},
+		{
+			label: 'Quit',
+			accelerator: 'CmdOrCtrl+Q',
+			click: () => {
+				app.quit();
+			},
+		},
+	]));
+
+	tray.on('click', initiateShowModal);
 }
 
 app.on('ready', () => {
